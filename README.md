c8d93dde1f631b1137c7445861ed72c1aeccb231
Duncan Cameron <phplist@dcameron.me.uk>
2023-04-28 08:11:41 +0100

    Based on existing code in the Addons plugin

---
+++ README.md
@@ -0,0 +1,19 @@
+# Updater Plugin #
+
+## Description ##
+This plugin provides a page to update the phplist code.
+
+## Installation ##
+
+### Dependencies ###
+
+This plugin requires phplist 3.5.4 or later, requires php version 7 or later, and also requires the Common plugin to be enabled.
+
+### Install through phplist ###
+The recommended way to install is through the Plugins page (menu Config > Manage plugins) using the package URL
+`https://github.com/bramley/phplist-plugin-updater/archive/master.zip`.
+
+## Version history ##
+
+    version         Description
+    1.0.0+20230428  Based on updater in the Addons Plugin
---
+++ plugins/UpdaterPlugin/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bramley/phplist-plugin-updater',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'phplist-plugin',
+        'install_path' => __DIR__ . '/../../../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bramley/phplist-plugin-updater' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'phplist-plugin',
+            'install_path' => __DIR__ . '/../../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/filesystem' => array(
+            'pretty_version' => 'v3.4.0',
+            'version' => '3.4.0.0',
+            'reference' => 'de56eee71e0a128d8c54ccc1909cdefd574bad0f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/filesystem',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
